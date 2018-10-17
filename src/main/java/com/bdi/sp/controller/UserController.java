423a40424c2198e1a05467674fadfd9610ee38fb
a <a@192.168.200.169>
2018-10-18 02:35:39 +0900

    Back

--- src/main/java/com/bdi/sp/controller/UserController.java
+++ src/main/java/com/bdi/sp/controller/UserController.java
@@ -12,6 +12,42 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
+	
+	
+	@RequestMapping(value="/login", method=RequestMethod.PUT)
+	public @ResponseBody Map<String,String> loginUp(@RequestBody Map<String,String> Uper){
+		Map<String,String> rMap = new HashMap<String,String>();
+		rMap.put("login","fail");
+		rMap.put("msg", "수정이안댐요");
+		if(Uper.get("id")==null) {
+			return rMap;
+		}
+		
+		if(Uper.get("name").equals("test")) {
+			if(Uper.get("id").equals("test")) {
+				if(Uper.get("pwd").equals("test")) {
+					if(Uper.get("pwdcheck").equals("test")) {
+						if(Uper.get("email").equals("test")) {
+							if(Uper.get("address").equals("test")) {
+								if(Uper.get("gender").equals("여")) {
+									if(Uper.get("birth").equals("test")) {
+										if(Uper.get("recommender").equals("test")) {
+											rMap.put("login", "success");
+											rMap.put("msg","수정성공.");
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return rMap;
+	}
+	
+	
+	
 
 	@RequestMapping(value="/login", method=RequestMethod.POST)
 	public @ResponseBody Map<String,String> login(@RequestBody Map<String,String> user){
@@ -22,8 +58,8 @@ public class UserController {
 			return rMap;
 		}
 		
-		if(user.get("id").equals("test")) {
-			if(user.get("pwd").equals("test")) {
+		if(user.get("id").equals("test1")) {
+			if(user.get("pwd").equals("test1")) {
 				rMap.put("login", "success");
 				rMap.put("msg","로그인 되었습니다.");
 			}
